b97a089effabcc9f42e64f86c1162885ecd152e6
ferdi7o <ferdim7o@gmail.com>
2020-12-28 13:04:47 +0200

    versiyon 1 While dongusu eklendi

--- sayi_tahminV1.py
+++ sayi_tahminV1.py
@@ -1,11 +1,16 @@
 print("Bil bakalim kac sayisini tuttum (0-100)?")
 tahmin_sayisi = 23
 
-kullanici = int(input("Tahmin Sayiniz:"))
 
-if (tahmin_sayisi > kullanici):
-    print("Daha Buyuk!")
-elif (tahmin_sayisi < kullanici):
-    print("Daha Kucuk!")
-elif (tahmin_sayisi == kullanici):
-    print("Tebrikler Bildiniz...")
+while True:
+    kullanici = int(input("Tahmin Sayiniz:"))
+    if (tahmin_sayisi > kullanici):
+         print("Daha Buyuk!")
+    elif (100 < kullanici):
+        print("Lutfen 100'den buyuk sayi girmeyin!")
+    elif (tahmin_sayisi < kullanici):
+        print("Daha Kucuk!")
+    elif (tahmin_sayisi == kullanici):
+        print("Tebrikler Bildiniz...")
+        break
+    
