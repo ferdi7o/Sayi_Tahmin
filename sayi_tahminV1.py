d15220400752adeb1430de912a9d4dda52251cd1
ferdi7o <ferdim7o@gmail.com>
2020-12-28 15:05:35 +0200

    save players and scores to .txt file

--- sayi_tahminV1.py
+++ sayi_tahminV1.py
@@ -1,9 +1,10 @@
 def sayitahmin():
     import random
-
-    print("Bil bakalim kac sayisini tuttum (0-100)?")
+    name = input("Isminizi giriniz:")
+    print( name ,"bil bakalim kac sayisini tuttum (0-100)?")
     tahmin_sayisi = random.randint(0,100)
     liste = []
+    players = open("players.txt","a")
 
     while True:
         kullanici = int(input("Tahmin Sayiniz:"))
@@ -11,16 +12,14 @@ def sayitahmin():
 
         if (tahmin_sayisi > kullanici):
              print("Daha Buyuk!",liste ,"\n")
-        elif (5 == len(liste)):
-            print("Kaybettiniz!\nTuttugum sayi:",tahmin_sayisi,"Sizin tahmin sayilariniz:",liste)
-            break
-
         elif (100 < kullanici):
             print("Lutfen 100'den buyuk sayi girmeyin!")
         elif (tahmin_sayisi < kullanici):
             print("Daha Kucuk!",liste , "\n")
         elif (tahmin_sayisi == kullanici):
-            print("Tebrikler Bildiniz..." , len(liste) ," denemede buldunuz.")
+            print("Tebrikler Kazandın." , name , len(liste) ,"denemede buldu.")
+            ekle = (name + " - " + str(len(liste)) + "\n")
+            players.write(ekle)
             break
 
 
